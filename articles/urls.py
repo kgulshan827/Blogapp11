056dfb335efe8017362db99c9f3dc2bac65216bb
Nikhil Kumar <nikhilkumar654231@gmail.com>
2021-01-20 11:50:08 +0530

    [FIX]Multiple Changes

--- articles/urls.py
+++ articles/urls.py
@@ -1,17 +1,9 @@
 
 from django.urls import path
 from .import views
-from django.conf.urls.static import static
 
 urlpatterns = [
-
     path('',views.article_list,name="list"),
-
     path('<slug:slug>/',views.article_detail,name="detail"),
     path('create',views.create,name="create"),
-    
-    
-    
-
-
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import  render,redirect
 from django.http import HttpResponse
-from.models import Articles
+from .models import Articles
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
@@ -10,25 +10,21 @@ from django.db.models import Count
 
 def home (request):
     return render(request,'home.html')
+
+
 def article_list(request):
     articles=Articles.objects.all().order_by('date')
+    return render(request,'article_list.html',{'articles':articles,})
 
-    return render(request,'article_list.html',{'articles':articles,
-                                               })
-@login_required(login_url="/accounts/login")
+
 def article_detail(request,slug):
-
     articles=get_object_or_404(Articles,slug=slug)
-
-
     comments = articles.comments.filter(active=True)
     new_comment = None
     is_liked=False
     if articles.likes.filter(id=request.user.id).exists():
         
         is_liked=True
-    
-
     if request.method == 'POST':
         comment_form = forms.CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -44,15 +40,14 @@ def article_detail(request,slug):
             new_comment.save()
     else:
         comment_form = forms.CommentForm()
-
-
-
     return render(request,'article_detail.html',{'article':articles,
                                                  'comments': comments,
                                                  'new_comment': new_comment,
                                                  'comment_form': comment_form,
                                                  'is_liked':is_liked,
                                                  'total_likes':articles.total_likes() })
+
+
 @login_required(login_url="/accounts/login")
 def like_post(request):
     article = Articles.objects.get(id=request.POST.get('article_id'))
@@ -68,27 +63,23 @@ def like_post(request):
         is_liked=True
 
     return redirect('detail',article.slug)
-def author(request):
 
-    
+
+def author(request):    
     article=(Articles.objects.filter(author=request.user)).count()
     articles=Articles.objects.filter(author=request.user)
-    
-    
     return render(request,'author.html',{'article':article,
     'articles':articles})
+
+
 def create(request):
-
     if request.method == 'POST':
-
         form=forms.Articleform(request.POST,request.FILES)
         if form.is_valid():
             instance=form.save(commit=False)
             instance.author=request.user
-
             instance.save()
             return redirect('list')
-
     else:
       form=forms.Articleform()
     return render(request,'create.html',{'form':form})
